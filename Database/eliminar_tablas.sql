be47510f121629b008f7eff6b25340325cb6b337
mrojo <rojo-mariano@hotmail.com>
2024-10-02 07:44:39 +0200

    Eliminar Tablas

---
+++ Database/eliminar_tablas.sql
@@ -0,0 +1,23 @@
+-- 1. Eliminar datos de las tablas:
+DELETE FROM CalifFinalAtributo;
+DELETE FROM CalificacionFinal;
+DELETE FROM CalificacionLote;
+DELETE FROM CalifLoteAtributo;
+DELETE FROM EspecificacionAtributo;
+DELETE FROM Especificacion;
+DELETE FROM Ingreso;
+DELETE FROM Atributo;
+DELETE FROM Articulo;
+DELETE FROM Usuario;
+
+-- 2. Verificación de las tablas vacías con consultas SELECT:
+SELECT * FROM CalifFinalAtributo;
+SELECT * FROM CalificacionFinal;
+SELECT * FROM CalificacionLote;
+SELECT * FROM CalifLoteAtributo;
+SELECT * FROM EspecificacionAtributo;
+SELECT * FROM Especificacion;
+SELECT * FROM Ingreso;
+SELECT * FROM Atributo;
+SELECT * FROM Articulo;
+SELECT * FROM Usuario;
